679fe215a07dbbc0fd0c5408927276586addc28e
Billy <billygun27@gmail.com>
2019-03-01 01:20:31 +0700

    train yolo

--- keras-yolo/yolo_evaluate_model.py
+++ keras-yolo/yolo_evaluate_model.py
@@ -7,7 +7,8 @@ import matplotlib.image as mpimg
 
 import numpy as np
 import keras.backend as K
-from keras.layers import Input, Lambda
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Input, Lambda ,Reshape
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
@@ -21,15 +22,62 @@ def _main():
     val_path = '2007_val.txt'
     test_path = '2007_test.txt'
     log_dir = 'logs/000/'
-    classes_path = 'model_data/coco_classes.txt'
+    classes_path = 'model_data/voc_classes.txt'
     anchors_path = 'model_data/yolo_anchors.txt'
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
+    num_anchors = len(anchors)
 
     input_shape = (416,416) # multiple of 32, hw
 
+    with open(train_path) as f:
+        train_lines = f.readlines()
 
+    with open(val_path) as f:
+        val_lines = f.readlines()
+
+    with open(test_path) as f:
+        test_lines = f.readlines()
+
+    image_input = Input(shape=(416, 416, 3))
+    model = yolo_body(image_input, num_anchors//3, num_classes)
+    model.load_weights("model_data/trained_weights_final.h5")
+
+    
+    # class+5
+    yolo3 = Reshape((13, 13, 3, 25))(model.layers[-3].output)
+    yolo2 = Reshape((26, 26, 3, 25))(model.layers[-2].output)
+    yolo1 = Reshape((52, 52, 3, 25))(model.layers[-1].output)
+    
+
+    model = Model( input= model.input , output=[yolo3,yolo2,yolo1] )
+    #model.summary()
+
+    model.compile(
+        optimizer=Adam(lr=1e-3) , 
+        loss='categorical_crossentropy', metrics=['accuracy']
+    )
+
+    #print( len(test_lines) )
+    batch_size = 2
+    
+    trainX = []
+    trainY = []
+
+    for  num in data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes) : 
+        x , y = num
+        trainX.append(x)
+        trainY.append(y)
+
+
+    data_train_generator = ImageDataGenerator()
+
+    test_generator = data_train_generator.flow(trainX, trainY, batch_size=batch_size), #data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes)
+
+    eva = model.evaluate_generator(test_generator, 80)
+
+    print(eva)
 
 def get_classes(classes_path):
     '''loads the classes'''
@@ -57,18 +105,18 @@ def data_generator(annotation_lines, batch_size, input_shape, anchors, num_class
             if i==0:
                 np.random.shuffle(annotation_lines)
             image, box = get_random_data(annotation_lines[i], input_shape, random=True)
-            print("box shape")
-            print(box.shape)
+            #print("box shape")
+            #print(box.shape)
             image_data.append(image)
             box_data.append(box)
             i = (i+1) % n
         image_data = np.array(image_data)
         box_data = np.array(box_data)
         y_true = preprocess_true_boxes(box_data, input_shape, anchors, num_classes)
-        print("y len")
-        print(len(y_true))
-        print(y_true[0].shape)
-        yield [image_data, *y_true], np.zeros(batch_size)
+        #print("y len")
+        #print(len(y_true))
+        #print(y_true[0].shape)
+        yield image_data, y_true
 
 def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, num_classes):
     n = len(annotation_lines)
